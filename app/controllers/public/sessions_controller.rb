3574ea7411b2ead53435851f4de84051b5cce577
KanaKarimata <kana.lt22@gmail.com>
2022-08-05 08:56:09 +0000

    ログイン、新規登録ページ

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_deleted_user, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -17,8 +18,34 @@ class Public::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 
-  # protected
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: 'ゲストユーザーとしてログインしました！'
+  end
+  
+  protected
+  
+  def reject_deleted_user
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      if (@user.valid_password?(params[:user][:password]) && (@user.is_deleted == "退会"))
+        redirect_to new_user_registration_path, falsh: {session_alert: "退会済のため、再登録が必要です。"}
+      end
+    end
+  end
+
+  def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
