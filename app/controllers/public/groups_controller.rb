997e53349fe9153ab90297314765cccf93ce75e8
KanaKarimata <kana.lt22@gmail.com>
2022-08-06 15:01:26 +0000

    グループ詳細画面にグループメンバー一覧を表示させた

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,5 +1,5 @@
 class Public::GroupsController < ApplicationController
-  
+
   def index
     @group = Group.new
     @groups = Group.all
@@ -7,24 +7,26 @@ class Public::GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @group_member = @group.users
+    @owner = @group.owner
   end
-  
+
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
     @group.save
     redirect_to groups_path
   end
-  
+
   def update
     @group.update(group_params)
     redirect_to groups_path
   end
-  
+
   private
 
   def group_params
     params.require(:group).permit(:name, :introduction, :image)
   end
-  
+
 end
