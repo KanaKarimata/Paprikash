e0430e7da30bab4fca5971b87e10ffe5a59b34bd
KanaKarimata <kana.lt22@gmail.com>
2022-08-06 06:39:19 +0000

    レイアウト追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -16,5 +16,19 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image)
+  end
+
 end
